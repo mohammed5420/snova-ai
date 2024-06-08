fd64a24555dae264da87459d686983ce49c27d3d
mohammed5420 <mohamed.elbadwi.dev@gmail.com>
2024-06-08 16:02:38 +0400

    init

---
+++ index.ts
@@ -0,0 +1,72 @@
+import puppeteer from "puppeteer";
+import turndown from "turndown";
+import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2), {
+  alias: { q: "query" },
+  string: ["q"],
+});
+
+const question = args.q;
+
+if (!question) {
+  console.error("Please provide a question");
+  process.exit(1);
+}
+
+async function getInspirationChannels(question: string) {
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
+    await page.goto("https://fast.snova.ai/", {
+      waitUntil: "load",
+      timeout: 0,
+    });
+
+    const textArea = await page.waitForSelector("textarea");
+    if (textArea) {
+      await textArea.type(question);
+      const button = await page.waitForSelector(
+        '[data-testid="stChatInputSubmitButton"]',
+      );
+
+      if (!button) {
+        console.log("No button found");
+        return browser.close();
+      }
+
+      await button.click();
+    }
+
+    //TODO: sleep for 2 seconds to allow the chatbot to respond, i need to find a better way to do this
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    const chatResponseNode = await page.waitForSelector(
+      'div.stChatMessage img[alt="assistant avatar"] + div',
+    );
+
+    if (!chatResponseNode) {
+      console.log("No response found");
+      return browser.close();
+    }
+
+    const responseHTML = await page.evaluate(
+      (node) => node.innerHTML,
+      chatResponseNode,
+    );
+
+    const turndownService = new turndown();
+    const responseMarkdown = turndownService.turndown(responseHTML);
+    console.log("Response: ", responseMarkdown);
+    return browser.close();
+
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+getInspirationChannels(question);
